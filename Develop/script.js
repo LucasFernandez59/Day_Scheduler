a66a30c92ce78718dfa0d22ee3f9cc03e8459b77
LucasF <che.fernandez255@gmail.com>
2023-11-06 22:11:45 -0700

    added functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(function () {
+    const currentHour = dayjs().format('H');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  
+    function updateHourlyColor() {
+      $('.time-block').each(function () {
+        const blockHour = parseInt($(this).attr('id'));
+        if (blockHour === currentHour) {
+          $(this).addClass('present').removeClass('past future');
+        } else if (blockHour < currentHour) {
+          $(this).addClass('past').removeClass('present future');
+        } else {
+          $(this).addClass('future').removeClass('past present');
+        }
+      });
+    }
+  
+    function saveText() {
+      $('.saveBtn').on('click', function () {
+        const key = $(this).parent().attr('id');
+        const value = $(this).siblings('.description').val();
+        localStorage.setItem(key, value);
+      });
+    }
+  
+    function loadText() {
+      $('.time-block').each(function () {
+        const key = $(this).attr('id');
+        const value = localStorage.getItem(key);
+        $(this).find('.description').val(value);
+      });
+    }
+  
+    function updateDate() {
+      const dateElement = $('#date');
+      dateElement.text(currentDate);
+    }
+  
+    updateHourlyColor();
+    saveText();
+    loadText();
+    updateDate();
+    setInterval(updateDate, 3600000); // Update the date every minute
+  });
